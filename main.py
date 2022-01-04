59db880dc4496e698803ab2eb9830d5199c2463e
Truong Vu <truongvu0911nd@gmail.com>
2022-01-04 09:05:20 +0700

    Update change r

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ parser.add_argument('--save_dir', default="", type=str, help='where to save wand
 parser.add_argument('--config', default="config.yaml", type=str, help='wandb config file')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--n_epochs', default=200, type=int, help='num epochs')
-parser.add_argument('--norm_type', default="", type=str, help='norm type')
-parser.add_argument('--r', default=1.0, type=float, help='renorm param r')
+parser.add_argument('--r', default=None, type=float, help='renorm param r')
 parser.add_argument('--log_norm_state_every', default=100, type=int)
 parser.add_argument('--use_scheduler', action='store_true', help="use learning rate scheduler")
 parser.add_argument('--wandb_group', default="", type=str, help='wandb group')
@@ -45,7 +44,7 @@ else:
                      dir=args.save_dir, config=args.config)
 config = wandb.config
 if not args.resume:
-    config.update({"lr": args.lr, "n_epochs": args.n_epochs}, allow_val_change=True)
+    config.update({"lr": args.lr, "n_epochs": args.n_epochs, "model_kwargs": {"r": args.r}}, allow_val_change=True)
     config.use_scheduler = args.use_scheduler
     config.log_norm_state_every = args.log_norm_state_every
 print(config)
