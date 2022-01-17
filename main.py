1d5c2e05cb08a57fdf9201fc8fe8cb645f2e1357
Truong Vu <truongvu0911nd@gmail.com>
2022-01-17 16:42:00 +0700

    new project name

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from models import *
 from utils import naive_lip
 
 
-PROJECT_NAME = 'Lipschitz'
+PROJECT_NAME = 'ClipGrad'
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
