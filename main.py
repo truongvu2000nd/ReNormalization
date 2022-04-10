f318a96ff8543e2232b117d82b7df4436986d8d7
Truong Vu <truongvu0911nd@gmail.com>
2022-04-11 01:11:16 +0700

    svhn dataset

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ parser.add_argument('--resume', '-r', action='store_true',
 parser.add_argument('--proj_name', default="", type=str, help='wandb project name')
 parser.add_argument('--id', default="", type=str, help='wandb_id (if set --resume)')
 parser.add_argument('--save_dir', default="", type=str, help='where to save wandb logs locally')
+parser.add_argument('--dataset', default="cifar10", type=str, help='dataset')
 parser.add_argument('--config', default="config.yaml", type=str, help='wandb config file')
 parser.add_argument('--arch', default="ResNet18", type=str, help='model architecture')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -73,31 +74,51 @@ if not os.path.isdir('checkpoint'):
 # --------------------------------------------------
 # -----------------------Data-----------------------
 # --------------------------------------------------
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+if args.dataset == "cifar10":
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root=config.data_dir, train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=config.batch_size, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root=config.data_dir, train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(
+        root=config.data_dir, train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=config.batch_size, shuffle=True, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+    testset = torchvision.datasets.CIFAR10(
+        root=config.data_dir, train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+
+
+elif args.dataset == "svhn":
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    trainset = torchvision.datasets.SVHN(
+        root=config.data_dir, split='train', download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=config.batch_size, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.SVHN(
+        root=config.data_dir, split='test', download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
 
 
 # ---------------------------------------------------
